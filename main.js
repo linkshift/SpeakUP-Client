5caf06c7f0b89e5e4375d3baedc8682c57497ffe
Pavel Tvaladze <github@link-mail.cf>
2016-01-18 01:18:48 +0100

    New features

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ const electron = require('electron');
 const ipcMain = electron.ipcMain;
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const BrowserOptions = {"extraHeaders" : "pragma: no-cache\n"}
+const BrowserOptions = {'extraHeaders' : 'pragma: no-cache\n'}
 
 let mainWindow;
 let ElectronSettings = require('electron-settings');
 let settings = new ElectronSettings();
+let callModeEnabled = false;
 
-function createWindow () {
+function callModeEnable() {
+	// mainWindow
+}
+
+function createWindow() {
 	var winWidth = typeof settings.get('app.width') == 'undefined' ?
 		1024 : settings.get('app.width');
 
@@ -26,31 +31,31 @@ function createWindow () {
 	var defaultSettings = {
 		minWidth: 640,
 		minHeight: 360,
-		windowWidth: 1024,
-		windowHeight: 600,
+		width: 1024,
+		height: 600,
 		center: true,
-		icon: __dirname + "/images/favicon.png",
-		title: "SpeakUP"
+		icon: __dirname + '/images/favicon-32x32.png',
+		title: 'SpeakUP'
 	}
 
 	if (winPosSaved) {
 		var winPosLeft = settings.get('app.left');
 		var winPosTop = settings.get('app.top');
 
-		defaultSettings.windowWidth = winWidth;
-		defaultSettings.windowHeight = winHeight;
+		defaultSettings.width = winWidth;
+		defaultSettings.height = winHeight;
 		defaultSettings.x = winPosLeft;
 		defaultSettings.y = winPosTop;
 		defaultSettings.center = false;
 	} else {
-		defaultSettings.windowWidth = winWidth;
-		defaultSettings.windowHeight = winHeight;
+		defaultSettings.width = winWidth;
+		defaultSettings.height = winHeight;
 		defaultSettings.center = true;
 	}
 
 	mainWindow = new BrowserWindow(defaultSettings);
 
-	mainWindow.setMenu(null);
+	mainWindow.setMenuBarVisibility(false);
 
 	mainWindow.on('closed', function() {
 		mainWindow = null;
@@ -62,6 +67,16 @@ function createWindow () {
 
 	mainWindow.on('unmaximize', function() {
 		settings.set('app.maximized', false);
+	});
+
+	mainWindow.on('minimize', function() {
+		console.log('Window has been minimized', 'callModeEnabled: ', callModeEnabled);
+
+		e.returnValue = false;
+	});
+
+	mainWindow.on('restore', function() {
+		console.log('Window has been restored', 'callModeEnabled: ', callModeEnabled);
 	});
 
 	mainWindow.on('resize', function(e) {
@@ -84,10 +99,14 @@ function createWindow () {
 		mainWindow.maximize();
 	}
 
-	mainWindow.loadURL("file://" + __dirname + "/check-connection.html");
-	// mainWindow.webContents.openDevTools();
+	mainWindow.loadURL('file://' + __dirname + '/check-connection.html');
+	mainWindow.webContents.openDevTools();
 
 	var webContents = mainWindow.webContents;
+
+	webContents.on('did-fail-load', function(e) {
+		console.log('load error', e);
+	});
 }
 
 app.on('ready', createWindow);
@@ -105,8 +124,24 @@ app.on('activate', function () {
 
 ipcMain.on('online-status-changed', function(event, status) {
 	if (status == 'online') {
-		mainWindow.loadURL("https://speakup.cf/", BrowserOptions);
+		var room = '';
+		for (var i = 0; i < process.argv.length; i++) {
+			if (process.argv[i].indexOf('speakup://') != -1) {
+				room = '/c/' + process.argv[i].replace('speakup://', '');
+				break;
+			}
+		}
+		mainWindow.loadURL('https://speakup.cf/' + room, BrowserOptions);
 	} else {
-		mainWindow.loadURL("file://" + __dirname + "/no-connection.html");
+		mainWindow.loadURL('file://' + __dirname + '/no-connection.html');
+	}
+});
+
+ipcMain.on('call-mode-change', function(event, status) {
+	console.log('Call mode changed to: ', status);
+	if (status == 'enabled') {
+		callModeEnabled = true;
+	} else {
+		callModeEnabled = false;
 	}
 });
